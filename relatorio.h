3ca58f9fb1147dc6db82a86f50845ee789a529b9
Marcio V <marcio203v1@gmail.com>
2025-10-06 16:16:52 -0300

    Create relatorio.h

---
+++ relatorio.h
@@ -0,0 +1,28 @@
+#ifndef relatorio_h
+#define relatorio_h
+
+#include "aluno.h"
+#include "professor.h"
+#include "disciplina.h"
+
+void Rlistaralunos(aluno a[], int *qtdaluno);
+void Rlistarprofessor(professor p[], int *qtdprof);
+void Rlistardisciplina(disciplina d[], int *qtdisciplina, professor p[], int *qtdprof);
+void Ralunosdisciplina(aluno a[], int *qtdaluno, disciplina d[], int *qtdisciplina, professor p[], int *qtdprof);
+void Rsexoalunos(aluno a[], int *qtdaluno);
+void Rnomealunos(aluno a[], int *qtdaluno);
+void Ralunosdatanascimento(aluno a[], int *qtdaluno);
+void Rprofessorsexo(professor p[], int *qtdprof);
+void Rnomeprofessor(professor p[], int *qtdprof);
+void Rprofessordatanascimento(professor p[], int *qtdprof);
+void Raniversariantesmes(aluno a[], int *qtdaluno, professor p[], int *qtdprof);
+void Ralunos3disciplinas(aluno a[], int *qtdaluno, disciplina d[], int *qtdisciplina);
+void Rdisciplinas40(disciplina d[], int *qtdisciplina, professor p[], int *qtdprof);
+void meses(aluno a[], int qtdaluno, professor p[], int qtdprof, int mes);
+int Pcomparadata(professor i, professor j);
+int Acomparadata(aluno i, aluno j);
+int posprof(professor p[], int qtdprof, int matricula);
+int posdisc(disciplina d[], int qtdisciplina, char codigo[]);
+int posaluno(aluno a[], int qtdaluno, int matricula);
+
+#endif
