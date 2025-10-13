eaef125ace5ed7f531bf3bc74a228b1ee36318d2
Marcio V <marcio203v1@gmail.com>
2025-10-13 23:57:01 +0000

    ok

--- Projeto/professor.c
+++ Projeto/professor.c
@@ -166,6 +166,11 @@ int validata(int dia, int mes, int ano) {
 
 int validarCPF(professor p[], char cpf[]){
     int i = 0;
+    for(int i=0; i<15; i++){
+        if(isdigit(p[i].cpf ) > 12){
+            return 0;
+        }
+    }
     while(*p[i].cpf != '\0'){      
         if(!isdigit(*p[i].cpf)){     
             return 0;             
