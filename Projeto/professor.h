78422df4be8ef1508905af9bcb18d7a3b33cdbe7
Marcio V <marcio203v1@gmail.com>
2025-10-14 15:19:59 -0300

    Update professor.h

--- Projeto/professor.h
+++ Projeto/professor.h
@@ -18,6 +18,6 @@ int atualizarprof(professor p[], int *qtdprof);
 int excluirprof(professor p[], int *qtdprof);
 int posprof(professor p[], int qtdprof, int matricula);
 int validata(int dia, int mes, int ano);
-int validarCPF(professor p[], char cpf[]);
+int validacpf(professor p[], int i);
 
 #endif
