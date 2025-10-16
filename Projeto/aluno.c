777080897da286f21811e836be809f19cc6ca813
Marcio V <marcio203v1@gmail.com>
2025-10-16 18:56:02 -0300

    Refactor aluno.c for improved readability

--- Projeto/aluno.c
+++ Projeto/aluno.c
@@ -4,8 +4,7 @@
 #include "aluno.h"
 
 int posaluno(aluno a[], int qtdaluno, int matricula){
-    int i;
-    for(i = 0; i < qtdaluno; i++){
+    for(int i = 0; i < qtdaluno; i++){
         if(a[i].matricula == matricula){
             return i;
         }
@@ -14,13 +13,13 @@ int posaluno(aluno a[], int qtdaluno, int matricula){
 }
 
 int validaData(int dia, int mes, int ano) {
-    if (ano < 1 || mes < 1 || mes > 12 || dia < 1) return 0;
-
+    if (ano < 1 || mes < 1 || mes > 12 || dia < 1){
+        return 0;
+    }
     int diasMes[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     if (mes == 2 && ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0)) {
         diasMes[2] = 29; // fevereiro em ano bissexto
     }
-
     return (dia <= diasMes[mes]) ? 1 : 0;
 }
 
@@ -39,7 +38,6 @@ int cadastraraluno(aluno a[], int *qtdaluno){
         else if(matricula < 0){
             return 2;
         }else{
-            
             a[*qtdaluno].matricula = matricula;
                 //Nome do aluno
             getchar();
@@ -51,7 +49,6 @@ int cadastraraluno(aluno a[], int *qtdaluno){
             scanf(" %c", &a[*qtdaluno].sexo);
             a[*qtdaluno].sexo = toupper(a[*qtdaluno].sexo);
             if(a[*qtdaluno].sexo != 'F' && a[*qtdaluno].sexo != 'M'){
-                
                 return 3;
             }
                 //CPF do aluno
@@ -66,10 +63,8 @@ int cadastraraluno(aluno a[], int *qtdaluno){
             scanf("%d/%d/%d", &a[*qtdaluno].dia, &a[*qtdaluno].mes, &a[*qtdaluno].ano);
             int res= validaData(a[*qtdaluno].dia, a[*qtdaluno].mes, a[*qtdaluno].ano);
             if(res == 0){
-                
                 return 5;
-            }
-                                    
+            }           
             a[*qtdaluno].ativo = 1;
             (*qtdaluno)++;
             return 6;                     
@@ -78,7 +73,6 @@ int cadastraraluno(aluno a[], int *qtdaluno){
 }
 
 int listaraluno(aluno a[], int qtdaluno){
-         //listar alunos
     if(qtdaluno == 0){
         return 0;
     }else{
@@ -88,7 +82,6 @@ int listaraluno(aluno a[], int qtdaluno){
                 printf("\nMatricula: %d  -  Nome: %s\n", a[i].matricula, a[i].nome);
                 printf("Sexo: %c  -  Data de Nascimento: %d/%d/%d\n", a[i].sexo, a[i].dia, a[i].mes, a[i].ano);
                 printf("CPF: %s\n", a[i].cpf);
-                                       
             }
         }
         return 1;
@@ -96,15 +89,13 @@ int listaraluno(aluno a[], int qtdaluno){
 }
 
 int atualizaraluno(aluno a[], int *qtdaluno){
-        //atualizar alunos
     int atualizarM;
     if(*qtdaluno == 0){
         return 0;
     }
     printf("Informe a matricula do aluno que você deseja atualizar: ");
     scanf("%d", &atualizarM);
-    int posicao = posaluno(a, *qtdaluno, atualizarM);
-                            
+    int posicao = posaluno(a, *qtdaluno, atualizarM);    
     if(posicao == -1){
         return 1;
     }else{
@@ -113,8 +104,7 @@ int atualizaraluno(aluno a[], int *qtdaluno){
             //Nome do novo aluno
         printf("\nNome: ");
         fgets(novoaluno.nome, sizeof(novoaluno.nome), stdin);
-        novoaluno.nome[strcspn(novoaluno.nome, "\n")] = '\0';
-                                
+        novoaluno.nome[strcspn(novoaluno.nome, "\n")] = '\0';   
             //Sexo do novo aluno
         printf("Sexo: 'F' ou 'M': ");
         scanf(" %c", &novoaluno.sexo);
@@ -144,7 +134,6 @@ int atualizaraluno(aluno a[], int *qtdaluno){
 }
 
 int excluiraluno(aluno a[], int *qtdaluno){
-        //excluir aluno
     int excluir;
     if(*qtdaluno == 0){
         return 0;
