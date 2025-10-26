2f29e97120a4c572032391546d6a9082bc7c49a4
Marcio V <marcio203v1@gmail.com>
2025-10-26 18:16:26 -0300

    Clean up trabalho1.h by removing comments
    Removed commented-out code for clarity.

--- trabalho1/trabalho1.h
+++ trabalho1/trabalho1.h
@@ -21,7 +21,7 @@
 // #################################################
 #ifndef TRABALHO1_H
 #define TRABALHO1_H
-
+/*
 typedef struct DQ
 {
     int iDia; 
@@ -42,13 +42,13 @@ typedef struct Qtd
 
 int somar(int x, int y); //função utilizada para testes
 int fatorial(int x); //função utilizada para testes
-int q1(char data[]);
-DiasMesesAnos q2(char datainicial[], char datafinal[]);
+*/int q1(char data[]);
+/*DiasMesesAnos q2(char datainicial[], char datafinal[]);
 int q3(char *texto, char c, int isCaseSensitive);
 int q4(char *strTexto, char *strBusca, int posicoes[30]);
 int q5(int num);
 int q6(int numerobase, int numerobusca);
 int q7(char matriz[8][10], char palavra[5]);
 DataQuebrada quebraData(char data[]);
-
+*/
 #endif  // MAIN_H
