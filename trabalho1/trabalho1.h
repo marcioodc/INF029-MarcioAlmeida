b50c9a9dd15fc897c55da4e11ce7baee94058858
Marcio V <marcio203v1@gmail.com>
2025-10-30 07:34:44 -0300

    Update trabalho1.h

--- trabalho1/trabalho1.h
+++ trabalho1/trabalho1.h
@@ -30,22 +30,22 @@ typedef struct DQ
     int valido; // 0 se inválido, e 1 se válido 
 
 } DataQuebrada;
-
+*/
 typedef struct Qtd
 {
-    int qtdDias;
-    int qtdMeses;
-    int qtdAnos;
+    int qtd_dias;
+    int qtd_meses;
+    int qtd_anos;
     int retorno;
 
-} DiasMesesAnos;
+} diasmesesanos;
 
-int somar(int x, int y); //função utilizada para testes
-int fatorial(int x); //função utilizada para testes
-*/int q1(char data[]);
-/*DiasMesesAnos q2(char datainicial[], char datafinal[]);
+//int somar(int x, int y); //função utilizada para testes
+//int fatorial(int x); //função utilizada para testes
+int q1(char data[]);
+DiasMesesAnos q2(char datainicial[], char datafinal[]);
 int q3(char *texto, char c, int isCaseSensitive);
-int q4(char *strTexto, char *strBusca, int posicoes[30]);
+/*int q4(char *strTexto, char *strBusca, int posicoes[30]);
 int q5(int num);
 int q6(int numerobase, int numerobusca);
 int q7(char matriz[8][10], char palavra[5]);
