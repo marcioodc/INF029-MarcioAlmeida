3c107efeafcbb2e401d60ba3bc1241c06f862585
Marcio V <marcio203v1@gmail.com>
2025-11-07 21:24:37 -0300

    Update MarcioAlmeida-20232160039-Q8.c

--- trabalho1/MarcioAlmeida-20232160039-Q8.c
+++ trabalho1/MarcioAlmeida-20232160039-Q8.c
@@ -3,13 +3,13 @@
 
 void main(){
   char tabuleiro[10][10];
-  int i, j, linha, coluna;
+  int linha, coluna;
   char jogador = 'X';
   int jogadas = 0;
   int venceu = 0;
 
-  for(int i=0; i< 10; i++){
-    for(int j=0; j<10; j++){
+  for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
       tabuleiro[i][j] = ' ';
     }
   }
