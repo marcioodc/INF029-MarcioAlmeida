2d26003addb20d0d2ed389373cb35bb05be2e945
Marcio V <marcio203v1@gmail.com>
2025-12-10 21:08:17 -0300

    Enhance dynamic array allocation and initializationtrabalho 2
    Added comments and structure for dynamic array management.

---
+++ teste.c
@@ -0,0 +1,24 @@
+ponteiro de vetor para os auxiliares
+0-9 usuario 1-10
+tam=5 posição -3
+antes de alocar tem que verificar se esta vazio
+inicializar todo o vetor
+int *a; int i;
+guardar os tamanhos em um vetor para se necessário utilizar-los depois
+  vetor TAM variavél global inicializa-los com sentinela
+contar quantos elementos há em cada vetor
+necessário um vetor de contador inicializados com 0, para cada espaço de vetor 
+ou
+utilizar ums struct{
+  int * p;
+  int cont;
+  int tam;
+}item;
+inicializar
+item *vp(10);
+item vp(10);
+//-> a=null; 
+posicao--;
+if(vp[posicao] == null){
+vp[posicao] == malloc(tam * sizeof(int));
+vetortam[posicao] == tam;
