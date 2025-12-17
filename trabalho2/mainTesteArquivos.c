7e18df5cd2fc103c6d94feefbee147cb2ebc0175
Marcio V <marcio203v1@gmail.com>
2025-12-17 11:47:30 +0000

    ok

--- trabalho2/mainTesteArquivos.c
+++ trabalho2/mainTesteArquivos.c
@@ -21,7 +21,7 @@ int main()
 {
     inicializar();
     testarArquivoPrimeiraVez();
-    testarArquivoSegundaVez();
+   // testarArquivoSegundaVez();
     /*testeInserirSemNada();
     testeCriarEstrutura();
     testeInserirComEstrutura();
