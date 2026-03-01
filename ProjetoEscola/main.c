b62bcbee6a62799528ab0e473d00593b0aef1e44
Marcio Almeida <marcio203v1@gmail.com>
2026-03-01 00:46:33 -0300

    ok

--- ProjetoEscola/main.c
+++ ProjetoEscola/main.c
@@ -1,9 +1,72 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include "aluno.h"
+
+
+int menu_principal();
+int menu_aluno();
+int menu_professor();
+int menu_disciplina();
+
+int main(){
+  int op, op_aluno, op_prof, op_disciplina;
+  while(1){
+    switch(menu_principal()){
+      case 0:
+        printf("Saindo do programa...\n");
+        return 0;
+      case 1:
+        switch(menu_aluno()){
+          case 0:
+            break;
+          case 1:
+            cadastrar_aluno();
+            break;
+          case 2:
+            atualizar_cadastro_aluno();
+            break;
+          case 3:
+            excluir_cadastro_aluno();
+            break;
+          default:
+            printf("Opção inválida. Tente novamente.\n");
+        }
+        break;
+      case 2:
+        switch(menu_professor()){
+          case 0:
+            break;
+          case 1:
+            cadastrar_professor();
+            break;
+          case 2:
+            atualizar_cadastro_professor();
+            break;
+          case 3:
+            excluir_cadastro_professor();
+            break;
+          default:
+            printf("Opção inválida. Tente novamente.\n");
+        }
+        break;
+      case 3:
+        switch(menu_disciplina()){
+          case 0:
+            break;
+          // Adicione as opções para disciplina aqui
+          default:
+            printf("Opção inválida. Tente novamente.\n");
+        }
+        break;
+      // Adicione o menu para relatório aqui
+      default:
+        printf("Opção inválida. Tente novamente.\n");
+    }
+  }
+}
 
 int menu_principal(){
+  int op;
   printf("\n>>>Projeto Escola<<<\n");
   printf("Escolha uma opcção:\n");
   printf("0 > Sair\n");
@@ -16,6 +79,7 @@ int menu_principal(){
 }
 
 int menu_aluno(){
+  int op_aluno; 
   printf("\n>>>MENU ALUNO<<<\n");
   printf("0 > Voltar\n");
   printf("1 > Cadastrar aluno\n");
@@ -26,6 +90,7 @@ int menu_aluno(){
 }
 
 int menu_professor(){
+  int op_prof;
   printf("\n>>>MENU PROFESSOR<<<\n");
   printf("0 > Voltar\n");
   printf("1 > Cadastrar professor\n");
@@ -37,4 +102,6 @@ int menu_professor(){
 
 int menu_disciplina(){
   printf("\n>>>MENU DISCIPLINA<<<\n");
+  printf("0 > Voltar\n");
+  printf("1 > Cadastar disciplina\n");
 }
