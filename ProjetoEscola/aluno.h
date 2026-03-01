75aae021a48a8eb7e4492e5dc06be5aa71f93934
MarcioAlmeida <marcio203v1@gmail.com>
2026-02-28 21:51:29 -0300

    26/02
    Added header guard and defined aluno structure.

--- ProjetoEscola/aluno.h
+++ ProjetoEscola/aluno.h
@@ -1,1 +1,13 @@
-#include <stdio.h>
+#ifdef ALUNO_H
+#define ALUNO_H
+#define TAM_ALUNO 2
+
+typedef struct aluno{
+    int matricula;
+    char nome[100];
+    char cpf[15];
+    char sexo[3];
+    int dia, mes, ano;
+}aluno;
+
+#endif
