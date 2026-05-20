a7c22cccef30a4c1928d3505f871464855e529e1
Marcio Almeida <marcio203v1@gmail.com>
2026-05-20 04:26:39 -0300

    ok

--- trabalho1/Q8.c
+++ trabalho1/Q8.c
@@ -11,22 +11,22 @@ O jogador deve informar a célula de interesse: ex: B3. O programa deve verifica
 se é uma célula válida
 O programa deve informar qual foi o ganhador, ou se não houve ganhador
 Deve ter apresentação do tabuleiro a cada jogada
-colunas: A B C
-linhas: 1 2 3
-Faça divida o problema em pequenas funções, e utilize o conceito de modularização. O programa deve ser escrito em C. O código deve ser bem documentado, com comentários explicando cada função e cada parte do código. O programa deve ser testado com diferentes jogadas para garantir que está funcionando corretamente.
 */
 
 void tabuleiro(char matriz[3][3])
 {
-    printf("%c | %c | %c \n", matriz[0][0], matriz[0][1], matriz[0][2]);
-    for (int m = 0; m < 10; m++)
+    printf("\n\t    1   2   3 \n\t");
+    printf("A   %c | %c | %c \n", matriz[0][0], matriz[0][1], matriz[0][2]);
+    printf("\t");
+    for (int m = 0; m < 15; m++)
         printf("-");
-    printf("\n");
-    printf("%c | %c | %c \n", matriz[1][0], matriz[1][1], matriz[1][2]);
-    for (int m = 0; m < 10; m++)
+    printf("\n\t");
+    printf("B   %c | %c | %c \n", matriz[1][0], matriz[1][1], matriz[1][2]);
+    printf("\t");
+    for (int m = 0; m < 15; m++)
         printf("-");
-    printf("\n");
-    printf("%c | %c | %c \n", matriz[2][0], matriz[2][1], matriz[2][2]);
+    printf("\n\t");
+    printf("C   %c | %c | %c \n", matriz[2][0], matriz[2][1], matriz[2][2]);
 }
 int main()
 {
@@ -40,6 +40,10 @@ int main()
         }
     }
     int vez = 1;
+    printf("\n");
+    printf("\n=================================\n");
+    printf("\tJOGO DA VELHA\n");
+    printf("=================================\n");
     for (int i = 0; i < 9; i++)
     {
         char jogador1 = 'X';
@@ -47,7 +51,8 @@ int main()
         char jogada[3];
         tabuleiro(matriz);
         char jogador = (i % 2) == 0 ? jogador1 : jogador2;
-        printf("Jogador %d\nInforme sua jogada: ", vez);
+        printf("\n=== Jogador %d ===\n", vez);
+        printf("\nInforme sua jogada: ");
         scanf("%s", jogada);
         int linha = toupper(jogada[0]) - 'A';
         int coluna = jogada[1] - '1';
@@ -90,11 +95,15 @@ int main()
     tabuleiro(matriz);
     if (ganhou)
     {
-        printf("O jogador %d ganhou!", vez);
+        printf("\n\n=================================\n");
+        printf("\tJOGADOR %d VENCEU!\n", vez);
+        printf("=================================\n");
     }
     else
     {
-        printf("Empate!");
+        printf("\n\n=================================\n");
+        printf("\tEMPATE!\n");
+        printf("=================================\n");
     }
     return 0;
 }
